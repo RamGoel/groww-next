8e71684b521659b20a495272cb31a6f073747603
Ram Goel <rgoel766@gmail.com>
2023-07-28 00:04:21 +0530

    fixed next error

--- app/layout.js
+++ app/layout.js
@@ -2,14 +2,13 @@
 import Header from '@components/Header'
 import '@styles/globals.css'
 import { Poppins } from 'next/font/google'
-import wrapper from '@redux/store'
 import { Providers } from '@redux/provider'
 const poppy = Poppins({ weight: "500", subsets: ['latin'] })
 
-export const metadata = {
-  title: 'SocioGram',
-  description: 'The one stop social media for developers',
-}
+// export const metadata = {
+//   title: 'SocioGram',
+//   description: 'The one stop social media for developers',
+// }
 
 export default function RootLayout({ children }) {
 
