7437e1c2e4bf6ab6b733d8dd7967c7b6fcd6ced2
Ram Goel <rgoel766@gmail.com>
2023-07-31 13:47:37 +0530

    make some changes

--- services/config.js
+++ services/config.js
@@ -2,6 +2,6 @@ import axios from "axios";
 export const Api = axios.create({
     baseURL: 'https://api.unsplash.com/',
     headers: {
-        "Authorization":`Client-ID SHoSJcKneTrl8NA3u-6S-_A7BYOVgMfD0dDz4Yn6_LI`
+        "Authorization":`Client-ID ${process.env.NEXT_PUBLIC_UNSPLASH_KEY}`
     }
 });
